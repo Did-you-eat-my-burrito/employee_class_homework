f234da4da8395e27abb4b1028e3152c77f006fb0
Did you eat my burrito <sean27up@gmail.com>
2019-06-23 21:58:06 +0900

    add code

--- main.py
+++ main.py
@@ -10,22 +10,23 @@ print("*==================================*")
 
 employees = []
 
-
-
-
 while True:
-    input_number = int(input("insert number"))
+
+    input_number = int(input("insert number >>>> "))
 
     if input_number == 1:
-        for info in employees:
+        
+        number = 0
 
-            print("Name:",info.name)
+        for info in employees:
+            print(number," :Name: ",info.name)
+            number += 1
 
     elif input_number == 2:
-        add_employee_name = input("insert employee's name")
-        add_employee_age = int(input("insert age"))
-        add_employee_phone_number = int(input("insert phone number"))
-        add_employee_id = int(input("insert id"))
+        add_employee_name = input("insert employee's name >>>> ")
+        add_employee_age = int(input("insert age >>>> "))
+        add_employee_phone_number = int(input("insert phone number >>>> "))
+        add_employee_id = int(input("insert id >>>> "))
         
         employee = Employee(add_employee_name,add_employee_age,add_employee_phone_number,add_employee_id)
         
@@ -33,18 +34,44 @@ while True:
         
         print("new employee added >>>> {}".format(employee.name))
 
-    #elif input_number == 3:
+    elif input_number == 3:
+
+        number = 0
+
+        for info in employees:
+            print(number," :Name: ",info.name)
+            number += 1
+
+        before_employee = int(input("insert number of employee to change"))
         
+        add_employee_name = input("insert new employee's name >>>> ")
+        add_employee_age = int(input("insert age >>>> "))
+        add_employee_phone_number = int(input("insert phone number >>>> "))
+        add_employee_id = int(input("insert id >>>> "))
+        
+        employee  = Employee(add_employee_name,add_employee_age,add_employee_phone_number,add_employee_id)
+        employees[before_employee] = employee
+        
+        print("[] is a added".format(add_employee_name))
+
     elif input_number == 4:
-        print(employees)
-        delete_employees = int(input("insert number "))
-        print("deleted{}".format(employees[delete_employees]))
+        
+        number = 0
+
+        for info in employees:
+            print(number," :Name: ",info.name)
+            number += 1
+        
+        
+        delete_employees = int(input("insert the number of employee to delete >>>> "))
+        print("{} is deleted ".format(employees[delete_employees]))
         employees.pop(delete_employees)
         
+    
     elif input_number == 5:
         break
 
     else:
-        print("wrong input")
+        print("this number does not exist")
 
 print("exit")
